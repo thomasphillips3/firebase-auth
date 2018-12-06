9c128b86c4799b2d5383e93dda66af46bf34abd2
Thomas Phillips <thomasphillips3@gmail.com>
2018-12-05 19:59:03 -0800

    Submit button creates a new user and texts them a code.

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-bbeae.cloudfunctions.net'
 
 class SignUpForm extends Component {
-    render() {
-        return (
-            <View>
-                <View style={{ marginBottom: 10 }}>
-                    <FormLabel>Enter phone number</FormLabel>
-                    <FormInput />
-                </View>
-                <Button title="Submit"></Button>
+  state = { phone: '' };
+
+  handleSubmit = async () => {
+      try {
+          await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+          await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
+        } catch (err) {
+            this.setState({ error: err });
+        }
+  }
+  
+  render() {
+      return (
+        <View>
+            <View style={{ marginBottom: 10 }}>
+                <FormLabel>Enter phone number</FormLabel>
+                <FormInput
+                    value={this.state.phone}
+                    onChangeText={ phone => this.setState({ phone }) }
+                    />
             </View>
+            <Button 
+                title="Submit"
+                onPress={this.handleSubmit} >
+            </Button>
+        </View>
         );
     }
 }
